2aa424ce596d038ab3177817871454435d0b484c
Pedro Ramos <pedroramosromero@gmail.com>
2025-02-21 14:55:07 +0100

    Update project

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
+  e2e: {
+    baseUrl: 'https://demoblaze.com/index.html',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
